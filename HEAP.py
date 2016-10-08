8bfd111046a67ddcfdcfcbb2d070a769b3e36d01
AAAAAzhong <zhongweijia2012@163.com>
2016-10-08 23:07:42 +0800

    heap sort

--- HEAP.py
+++ HEAP.py
@@ -6,7 +6,7 @@ import math
 class Heap(object):
 
 	def __init__(self,inputlist):
-		self._heaplist = inputlist
+		self.heaplist = inputlist
 		self.c = 0
 	
 	def _normal_minheap(self,inlist):
@@ -57,81 +57,121 @@ class Heap(object):
 
 	@property
 	def minheap(self):
-		self._normal_minheap(self._heaplist)
+		self._normal_minheap(self.heaplist)
 
-		return self._heaplist
+		return self.heaplist
 
 	@property
 	def maxheap(self):
-		self._normal_maxheap(self._heaplist)
+		self._normal_maxheap(self.heaplist)
 
-		return self._heaplist
+		return self.heaplist
 
-	@property
+	#@property
 	def fast_minheap(self):
-		n = len(self._heaplist)
+		n = len(self.heaplist)
 
 		for i in xrange(n/2,-1,-1):
-			self._sink_min(self._heaplist,i)
+			self._sink_min(self.heaplist,i)
 
-		return self._heaplist
+		return self.heaplist
+		#return Heap(self.heaplist)
 
-	@property
+	#@property
 	def fast_maxheap(self):
-		n = len(self._heaplist)
+		n = len(self.heaplist)
 	
 		for i in xrange(n/2,-1,-1):
-			self._sink_max(self._heaplist,i)
+			self._sink_max(self.heaplist,i)
 
-		return self._heaplist
-
-	def minheap_insert(self,data):
-		self._heaplist.append(data)
+		return self.heaplist
+		#return Heap(self.heaplist)
 	
-		n = len(self._heaplist)
+	def minheap_insert(self,data):
+		self.heaplist.append(data)
+	
+		n = len(self.heaplist)
 		
 		k = n
 		while k != 0:
 			k = k/2
-			self._sink_min(self._heaplist,k)
+			self._sink_min(self.heaplist,k)
 
 		
 
 	def maxheap_insert(self,data):
-		self._heaplist.append(data)
+		self.heaplist.append(data)
 
-		n = len(self._heaplist)
+		n = len(self.heaplist)
 		
 		k = n
 		while k!=0:
 			k = k/2
-			self._sink_max(self._heaplist,k)
+			self._sink_max(self.heaplist,k)
+
+	def _pop(self,inmode='max'):
+	
+		n = len(self.heaplist)
+		if n > 1:
+			temp = self.heaplist[0]
+			self.heaplist[0] = self.heaplist.pop()
+	
+			
+			if inmode == 'max':
+
+				while n!=0:
+					n = n/2
+					self._sink_max(self.heaplist,n)
+
+			else:
+				
+				while n!=0:
+					n = n/2
+					self._sink_min(self.heaplist,n)
+		else:
+			temp = self.heaplist.pop()
+			
+		return temp
+
+	def pop(self,mode):
+		return self._pop(inmode=mode)
+		
+		
+		
 
 def main():
-	inlist = [i for i in xrange(10,1,-1)]
-	test = Heap(inlist)
+	inlist = [i for i in xrange(1,10,1)]
+	inlist2 = [i for i in xrange(1,11,1)]
+	inlist3 = [2,3,1,4,6,2,7,1,4,8,92,45,26,73]
+	#test = Heap(inlist)
 	
-	print "minheap","."*30
-	for i in test.minheap:
-		print i
-	
-	print "fastminheap","."*30
-	for i in test.fast_minheap:
-		print i
+	#print "minheap","."*30
+	#for i in test.minheap:
+	#	print i
+	#
+	#print "fastminheap","."*30
+	#for i in test.fast_minheap:
+	#	print i
 
-	print "maxheap","."*30
-	for i in test.maxheap:
-		print i
+	#print "maxheap","."*30
+	#for i in test.maxheap:
+	#	print i
 
-	print "fastmaxheap","."*30
-	for i in test.fast_maxheap:
-		print i
+	#print "fastmaxheap","."*30
+	#for i in test.fast_maxheap:
+	#	print i
 
-	test.minheap_insert(11)
-	print test.minheap
+	#test.minheap_insert(11)
+	#print test.minheap
 
-	test.maxheap_insert(11)
-	print test.maxheap
+	#test.maxheap_insert(11)
+	#print test.maxheap
+	maxheap = Heap(inlist3)
+	maxheap.fast_maxheap()
+	while maxheap.heaplist:
+		print maxheap.pop("max")
+
+	#for i in xrange(5): print test.pop("max")
 
 if __name__ == '__main__':
 
